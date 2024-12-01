c4099570b993b80fc85dc23bce34fefdc99cb9d6
p-shrey10 <pradhanangashrey@gmail.com>
2024-12-01 12:09:59 +0545

    init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import About from "./components/About.svelte";
+  import Project from "./components/Project.svelte";
+  import Experience from "./components/Experience.svelte";
+  import Skill from "./components/Skill.svelte";
+  import Education from "./components/Education.svelte";
+  import Training from "./components/Training.svelte"
+  import Footer from './components/Footer.svelte'
+</script>
+
+<main>
+    <About/>
+    <Project/>
+    <Experience />
+    <Skill/>
+    <Education />
+    <Training />
+    <Footer />
+</main>
+
+<style></style>
