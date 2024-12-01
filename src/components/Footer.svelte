b8b935dc209792dde284503e6b58dc8fbdc2885b
p-shrey10 <pradhanangashrey@gmail.com>
2024-12-01 12:10:56 +0545

    footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { fly } from 'svelte/transition';
+  
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+  </script>
+  
+  <footer class="bg-gray-800 text-white py-6 mt-10">
+    <div class="container mx-auto px-4">
+      <div
+        class="flex justify-end items-center space-x-4"
+        in:fly={{ x: 100, duration: 500 }}
+      >
+        <div class="text-sm">
+          <p>&copy; {currentYear} Shrey Pradhananga. All Rights Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
